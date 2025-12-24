c330286872c76b8b062e34a273ce2ae53b4bd223
Long <dokimlong3010@gmail.com>
2025-12-24 08:12:52 +0700

    adding edit route

---
+++ app/api/issue/[id]/route.ts
@@ -0,0 +1,39 @@
+import { schema } from "@/app/validationSchema";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // Placeholder implementation
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(JSON.stringify({ errors: validation.error.format() }), {
+      status: 400,
+    });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json(JSON.stringify({ error: "Issue not found" }), {
+      status: 404,
+    });
+  }
+
+ const updatedIssue = await prisma.issue.update({
+    where: {id : issue.id},
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
